2509fe3d3ae0f17e21fb7a318e9b28f979771785
Nikita Polunin (二木立・半人) <77877550+Eienrojin@users.noreply.github.com>
2022-01-21 12:20:29 +0300

    Update Program.cs

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -11,11 +11,22 @@ namespace Homeworks
         static void Main(string[] args)
         {
             Matrix matrix = new Matrix(3, 3);
+            Matrix matrix1 = new Matrix(6, 6);
 
             matrix.InitRandomValues();
+            matrix1.InitRandomValues();
 
-            Console.WriteLine("Исходная матрица");
-            matrix.PrintMatrix(matrix.Values);
+            Console.WriteLine("Исходная матрица 1");
+            matrix.PrintMatrix();
+            Console.WriteLine("Исходная матрица 2");
+            matrix1.PrintMatrix();
+
+            matrix.Values = matrix.subtrMatrix(3);
+            matrix1.Values = matrix1.subtrMatrix(6);
+            Console.WriteLine("Измененная матрица 1");
+            matrix.PrintMatrix();
+            Console.WriteLine("Измененная матрица 2");
+            matrix1.PrintMatrix();
 
             Console.ReadKey();
         }

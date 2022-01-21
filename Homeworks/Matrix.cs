64c20a75bff9609baaa0d3b42d426a9c4530e06d
user <user@C1014>
2022-01-21 10:27:06 +0300

    Добавьте файлы проекта.

---
+++ Homeworks/Matrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks
+{
+    internal class Matrix
+    {
+        public double[,] values { get; set; }
+
+        public Matrix(int columns, int rows)
+        {
+            Columns = columns;
+            Rows = rows;
+
+            double[,] values = new double[Rows, Columns];
+        }
+
+        public int Columns { get; set; }
+        public int Rows { get; set; }
+        public double[,] MatrixValues { get; set; }
+
+        public void PrintMatrix(double[,] values)
+        {
+            for (int i = 0; i < Columns; i++)
+            {
+                for (int j = 0; j < Rows; j++)
+                {
+                    Console.Write($"{values[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homeworks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix(3, 3);
+
+            InitRandomValues(matrix.values);
+
+            Console.WriteLine("Исходная матрица");
+            matrix.PrintMatrix(matrix.values);
+
+            Console.ReadKey();
+        }
+
+        static double InitRandomValues(double[,] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i,j] = random.Next(1, 10);
+                    return array[i, j];
+                }
+            }
+        }
+    }
+}
